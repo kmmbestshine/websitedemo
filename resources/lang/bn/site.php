08da2875b04455e4104496b53c2f0242952d95d8
example1 <32572574+bestshine@users.noreply.github.com>
2020-08-28 23:22:24 +0530

    initial commit
    initial commit

---
+++ resources/lang/bn/site.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Website Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during website frontend
+    */
+
+    'menu_home' => 'ஹோம்',
+    'menu_class' => 'கிளாஸ்',
+    'menu_teachers' => 'ஆசிரியர்கள்',
+    'menu_events' => 'இவென்ட்',
+    'menu_gallery' => 'ஃபோட்டோ',
+    'menu_contact_us' => 'எங்களை தொடர்பு கொள்ளுங்கள்',
+    'menu_faq' => 'அடிக்கடி கேட்கப்படும் கேள்வி',
+    'menu_admission' => 'சேர்க்கை',
+    'menu_result' => 'தேர்வு முடிவு',
+    'menu_timeline' => 'காலவரிசை',
+    'empty_content' => 'வெற்று_ உள்ளடக்கம்',
+    'apply_now' => 'இப்பொழுது விண்ணப்பியுங்கள்',
+    'about_us' => 'எங்களை பற்றி',
+    'why_we' => 'ஏன் நாங்கள்',
+    'about_us_more' => 'எங்களைப் பற்றி மேலும்',
+    'about_us_more2' => 'எங்களைப் பற்றி மேலும்2',
+    'service' => 'சேவைகள்',
+    'stat_students' => 'மாணவர்களின் நிலை',
+    'stat_teachers' => 'ஆசிரியர்களின் நிலை',
+    'stat_college' => 'பள்ளி நிலை',
+    'stat_books' => 'புத்தகங்களின் நிலை',
+    'testimonials' => 'சான்றுகள்',
+    'get_in' => 'உள்ளே வா',
+    'drop_email' => 'மின்னஞ்சலை விடுங்கள்',
+    'write_email' => 'மின்னஞ்சல் எழுதுங்கள்',
+    'subscribe' => 'பதிவு',
+    'up_event' => 'அப் ஈவென்ட்',
+    'help_links' => 'உதவி இணைப்புகள் ',
+    'copy_right' => \App\Http\Helpers\AppHelper::translateNumber(date('Y')).' copy_right',
+    'maintainer' => 'பராமரிப்பாளர்',
+    'details' => 'விவரங்கள்',
+    'information' => 'தகவல்',
+    'teacher' => 'ஆசிரியர்',
+    'room_no' => 'அறை எண்',
+    'capacity' => 'திறன்',
+    'shift' => 'மாற்றம்',
+    'description' => 'விளக்கம்',
+    'course_outline' => 'கோர்ஸ்_அவுட்லைன் ',
+    'meet_teacher' => 'ஆசிரியரை சந்திக்கவும்',
+    'more' => 'மேலும்',
+    'gallery_title' => 'கேலரி_ தலைப்பு',
+    'contact_us_form_title' => 'எங்களை தொடர்பு கொள்ள தலைப்பு',
+    'yn' => 'உங்கள் பெயர்',
+    'ye' => 'உங்கள் மின்னஞ்சல்',
+    'subject' => 'சப்ஜெக்ட் ',
+    'ym' => 'உங்கள் தகவல்',
+    'send' => 'அனுப்புக',
+    'address' => 'முகவரி',
+    'phone_no' => 'தொலைபேசி எண்',
+    'email' => 'மின்னஞ்சல்',
+    'our_office' => 'எங்கள்_ அலுவலகம்',
+    'faq_title' => 'அடிக்கடி கேட்கப்படும் கேள்வி தலைப்பு',
+    'timeline_title' => 'காலவரிசை_ தலைப்பு',
+    'not_found' => 'கிடைக்கவில்லை',
+    'not_found_title' => 'தலைப்பு கிடைக்கவில்லை',
+    'code_500' => '৫০০',
+    'code_500_title' => 'সার্ভারের সমস্যা! দয়া করে সিস্টেম এডমিনের সাথে যোগাযোগ করুন।',
+
+
+
+
+];
